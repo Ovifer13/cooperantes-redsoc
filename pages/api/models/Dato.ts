431c2ee96ed69242e2ec54aa2e5187460ef4b589
Ovidio Rodriguez <ovifer02@gmail.com>
2022-08-15 17:03:38 -0400

    Dato model

---
+++ pages/api/models/Dato.ts
@@ -0,0 +1,44 @@
+const { DataTypes, Model } = require('sequelize')
+
+module.exports = (sequelize: any) => {
+
+    class Dato extends Model {}
+
+    Dato.init({
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        nombre: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        cooperante_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'cooperantes',
+                key: 'id'
+            }
+        },
+        universidad_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'universidades',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        modelName: 'datos',
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    })
+
+    return Dato
+
+}
+
+export {}
