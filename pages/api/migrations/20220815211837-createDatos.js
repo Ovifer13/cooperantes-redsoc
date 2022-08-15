7645c9cf5ca07a25cc8264de403d5ddb9d2c6f65
Ovidio Rodriguez <ovifer02@gmail.com>
2022-08-15 17:20:59 -0400

    fix createDatos migration

---
+++ pages/api/migrations/20220815211837-createDatos.js
@@ -0,0 +1,44 @@
+module.exports = {
+  
+  async up (queryInterface, Sequelize) {
+      return queryInterface.createTable('datos', {
+          id: {
+              type: Sequelize.DataTypes.INTEGER,
+              primaryKey: true,
+              autoIncrement: true,
+              allowNull: false
+          },
+          nombre: {
+              type: Sequelize.DataTypes.STRING,
+              allowNull: false
+          },
+          cooperante_id: {
+              type: Sequelize.DataTypes.INTEGER,
+              references: {
+                model: 'cooperantes',
+                key: 'id'
+              }
+          },
+          universidad_id: {
+              type: Sequelize.DataTypes.INTEGER,
+              references: {
+                model: 'universidades',
+                key: 'id'
+              }
+          },
+          catalogo_dato_id: {
+            type: Sequelize.DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+              model: 'universidades',
+              key: 'id'
+            }
+          }
+      })
+  },
+
+  async down (queryInterface) {
+      return queryInterface.dropTable('datos');
+  }
+
+}
