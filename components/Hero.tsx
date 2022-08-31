3a523e472190a63609ed20d67ea97cfef4e445a3
Ovidio Rodriguez <ovifer02@gmail.com>
2022-08-31 01:07:06 -0400

    Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,28 +1,63 @@
-import React from 'react'
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
-const Hero = ({}) => {
-    return (
-        <div className='Hero grid grid-cols-1 md:grid-cols-2'>
-            <div className='grid grid-rows-2 px-2 lg:px-4'>
-                <div className=''>
-                    <h1 className=''>
-                        { 'Text1' }
-                    </h1>
-                    <h1 className=''>
-                        { 'Text2' }
-                    </h1>
+const Hero = () => {
+  return (
+    <div className="relative md:mt-40 bg-white max-w-full">
+      <div className="mx-auto max-w-7xl">
+        <div className="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
+          <svg
+            className="absolute inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
+          >
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </svg>
+
+          <main className="mx-auto mt-10 max-w-7xl px-4 md:pt-20 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-10">
+            <div className="sm:text-center lg:text-left">
+              <h1 className="text-4xl font-bold mt-10 tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+                <span className="block xl:inline">Cooperantes</span>{' '}
+                <span className="block text-primary xl:inline">Redsoc</span>
+              </h1>
+              <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
+                Directorio de cooperantes afiliados a Redsoc
+              </p>
+              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                <div className="rounded-md shadow">
+                  <a
+                    href="cooperantes"
+                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-primary px-8 py-3 text-base font-medium text-white hover:bg-gray md:py-4 md:px-10 md:text-lg"
+                  >
+                    Cooperantes
+                  </a>
                 </div>
-                <div className='mt-8 xl:mt-0'>
-                    <p>
-                        { 'Text3' }
-                    </p>
+                <div className="mt-3 sm:mt-0 sm:ml-3">
+                  <a
+                    href="universidades"
+                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-primary px-8 py-3 text-base font-medium text-solid-white hover:bg-gray md:py-4 md:px-10 md:text-lg"
+                  >
+                    Universidades
+                  </a>
                 </div>
+              </div>
             </div>
-            <div className='grid px-2 lg:px-4 max-w-md md:max-w-lg place-content-center mt-12 md:mt-0 m-auto'>
-                <img src='/' alt='Hero' />
-            </div>
+          </main>
         </div>
-    )
+      </div>
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <img
+          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full"
+          src="https://user-images.githubusercontent.com/86666889/187567467-46571ff6-fcde-44ca-a8a2-fec7eb0416ee.JPG"
+          alt=""
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Hero
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,10 @@
 import type { NextPage } from 'next'
-import { useQuery } from '@apollo/client'
-
-import { GetTestData } from '../interfaces/TestInterfaces'
-
-import { GET_TEST } from '../operations/queries/TestQueries'
-import Section from '../components/Section'
 import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
 
-    const { error, data, loading } = useQuery<GetTestData>(
-        GET_TEST
-    )
-  
-    const {
-        getTest
-    } = {...data}
-
-    if (error || loading) (
-        <>
-        ...
-        </>
-    )
-
     return (
-        <h1 className='text-3xl font-bold'>
-            <Section>
-                <Hero />
-            </Section>
-        </h1>
+        <Hero />
     )
 }
 
