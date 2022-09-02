c9d542d5f81794862d6b130829a79f11bb327598
Ovidio Rodriguez <ovifer02@gmail.com>
2022-09-01 23:34:59 -0400

    Add variables to nextjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    POSTGRES_DB_HOST: process.env.POSTGRES_DB_HOST,
+    POSTGRES_DB_USERNAME: process.env.POSTGRES_DB_USERNAME,
+    POSTGRES_DB_PASSWORD: process.env.POSTGRES_DB_PASSWORD,
+    POSTGRES_DB_NAME: process.env.POSTGRES_DB_NAME,
+    POSTGRES_DB_PORT: process.env.POSTGRES_DB_PORT
+  }
 }
 
 module.exports = nextConfig
